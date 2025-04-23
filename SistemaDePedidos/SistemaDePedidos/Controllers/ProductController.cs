881d5dd33502a12c47ba639bb00bb8aea9ff0857
Aaron <sergio.bravo.9406@gmail.com>
2025-04-22 21:16:06 -0300

    Add files via upload

---
+++ SistemaDePedidos/SistemaDePedidos/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Views;
+using Models;
+using Controllers;
+
+
+namespace Controllers
+{
+    class ProductController
+    {
+        
+        public ProductController()
+        {
+        }
+
+        public Product LoadProduct()
+        {
+            Console.WriteLine("------LOADING PRODUCT------");
+            return ProductView.LoadProduct();
+        }
+        public static List<Product> LoadProductList()
+        {
+            string resp = "n";
+            List<Product> temp = new List<Product>();
+            do
+            {
+                temp.Add(ProductView.LoadProduct());
+                Console.WriteLine("Add another product? Y/N");
+                resp = Console.ReadLine();
+            } while (resp.ToLower() != "n");
+
+            return temp;
+        }
+
+        public void ShowProductList(List <Product>productList)
+        {
+            ProductView.ShowProductList(productList);
+        }
+
+
+
+
+    }
+}
