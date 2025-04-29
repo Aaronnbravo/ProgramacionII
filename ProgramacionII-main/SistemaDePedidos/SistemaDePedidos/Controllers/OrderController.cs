6e606b494b58482a73b3894047c25f864851b9a8
Aaron <sergio.bravo.9406@gmail.com>
2025-04-29 20:38:18 -0300

    Add files via upload

---
+++ ProgramacionII-main/SistemaDePedidos/SistemaDePedidos/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Controllers;
+using Models;
+using Views;
+using Repository;
+
+namespace Controllers
+{
+    class OrderController
+    {
+        private ClientController cController;
+        private ProductController pController;
+        private List<Order> orderList = new List<Order>();
+
+        public OrderController()
+        {
+            this.cController = new ClientController();
+            this.pController = new ProductController();
+            this.orderList = Repository<Order
+
+        }
+
+        public void CreateOrder()
+        {
+            Order temp = new Order();
+            temp.client = ClientView.LoadClient();
+            temp.setProductList(OrderView.LoadProductList());
+        }
+    }
+}
---
+++ ProgramacionII-main/SistemaDePedidos/SistemaDePedidos/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Views;
+using Controllers;
+using Models;
+namespace SistemaDePedidos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               
+        }
+
+        static void MenuPrincipal()
+        {
+            OrderController oController = new OrderController();
+            bool salir = false;
+            int opcion = 0;
+            Console.WriteLine("welcome to the ordering system");
+            Console.WriteLine("Enter the option you want to make");
+            Console.WriteLine("1_Show Products");
+            Console.WriteLine("2_Load Products");
+            Console.WriteLine("3_Show Clients");
+            Console.WriteLine("4_Load Clients");
+            Console.WriteLine("5_Show Order");
+            Console.WriteLine("6_Load Order");
+            Console.WriteLine("7_Exit");
+            opcion = int.Parse(Console.ReadLine());
+            do
+            {
+
+                switch (opcion)
+                {
+                    case 1:
+                        oController.CreateOrder();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+
+                }
+            } while (salir);
+
+        }
+    }
+}
